f6b92f35b182db1ad3c9b18edce034f2ec69fee2
Anuranjan <itanuranjan@gmail.com>
2024-05-12 23:56:04 +0530

    git push for production

---
+++ script.js
@@ -0,0 +1,8 @@
+const progressButton = document.getElementById('progressButton');
+    setTimeout(() => {
+        progressButton.classList.add('in-progress'); 
+    }, 2000); 
+const progressButton1 = document.getElementById('progressButton1');
+    setTimeout(() => {
+        progressButton1.classList.add('in-progress'); 
+    }, 2000); 
